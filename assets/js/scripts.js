4d2e0a95e94589ad2acb5dc1f9bdcd759f962d71
Joshua B. Turingan <turingan.joshua@gmail.com>
2016-10-21 16:56:14 +0800

    added portfolio module with no js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-	
-	$('main section').height($(window).outerHeight());
+
+	resize();
 
 	$(window).resize(function() {
-		$('main').height($(window).outerHeight());
-		$('main section').height($(window).outerHeight());
+		resize();
 	})
+
+
+	function resize() {
+		// $('main').css({ height: $(window).outerHeight()});
+		$('main section').css({ height: $(window).outerHeight()});
+	}
 })
