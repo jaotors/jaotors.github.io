c699048470efb9e24c0c658a85a062276b5384c4
Joshua Turingan <turingan.joshua@gmail.com>
2025-11-20 16:37:01 +0800

    Add animation on tech stack icons

--- index.js
+++
@@ -1,6 +0,0 @@
-;(() => {
-  const copyrightElem = document.querySelector('[data-id="copyright"]')
-  const yearNow = new Date().getFullYear()
-  const name = 'Jao Turingan'
-  copyrightElem.append(`${yearNow} ${name}`)
-})()
